d0e4131e3d3ca698a7c8a496aea246e2827cbf24
sdadha <92512653+sdadha@users.noreply.github.com>
2023-05-07 00:14:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import axios from 'axios';
-import Form from './Form'
+import React from "react";
+import axios from "axios";
+import Form from "./Form"
 
 function App() {
   return <Form/>;
